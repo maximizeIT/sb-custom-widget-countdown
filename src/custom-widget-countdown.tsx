c8bef4fa49b0d8437e75424425b847cf97c122bb
Max Scholz <max.scholz@maximize-it.eu>
2021-12-19 17:59:25 +0100

    update

--- src/custom-widget-countdown.tsx
+++ src/custom-widget-countdown.tsx
@@ -18,19 +18,29 @@ export interface CustomWidgetCountdownProps extends BlockAttributes {
 
 export const CustomWidgetCountdown = ({ title, showtitle, titlecolor, boxescolorbg, boxescolorborder, boxescolortext, countdowndate, expiredmessage, contentLanguage }: CustomWidgetCountdownProps): ReactElement => {
 
-  return <div><CountdownView
-    {...{
-      title,
-      showtitle,
-      titlecolor,
-      boxescolorbg,
-      boxescolorborder,
-      boxescolortext,
-      countdowndate,
-      expiredmessage,
-      contentLanguage,
-    }}
-  /><br/><br/><div data-widget-conf-grid-type="100" data-widget-type="Section" data-widget-src="internal://staffbase.content.widgets.Section"><div>
-  <div data-widget-conf-type="warning" data-widget-on-card="true" data-widget-type="Infobox" data-widget-src="internal://staffbase.content.widgets.Infobox"><p>PROTOTYPES AND FOR DEMO PURPOSES ONLY!</p><p>NOT FOR PRODUCTION USE!</p><p>NOT AVAILABLE TO CUSTOMERS!</p></div>
-  </div></div></div>
+  return <div>
+      <CountdownView
+      {...{
+        title,
+        showtitle,
+        titlecolor,
+        boxescolorbg,
+        boxescolorborder,
+        boxescolortext,
+        countdowndate,
+        expiredmessage,
+        contentLanguage,
+      }}
+    />
+    <br /><br />
+    <div data-widget-conf-grid-type="100" data-widget-type="Section" data-widget-src="internal://staffbase.content.widgets.Section">
+      <div>
+        <div data-widget-conf-type="warning" data-widget-on-card="true" data-widget-type="Infobox" data-widget-src="internal://staffbase.content.widgets.Infobox">
+          <p>PROTOTYPES AND FOR DEMO PURPOSES ONLY!</p>
+          <p>NOT FOR PRODUCTION USE!</p>
+          <p>NOT AVAILABLE TO CUSTOMERS!</p>
+        </div>
+      </div>
+    </div>
+  </div>
 };
