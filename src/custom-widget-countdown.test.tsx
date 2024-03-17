99cb6caa89a1971f680a21d94978211fba651606
Max Scholz <max@staffbase.com>
2024-03-17 14:13:43 +0100

    chore: housekeeping, remove hardcoded disclaimer

--- src/custom-widget-countdown.test.tsx
+++ src/custom-widget-countdown.test.tsx
@@ -5,8 +5,8 @@ import {CustomWidgetCountdown} from "./custom-widget-countdown";
 
 describe("CustomWidgetCountdown", () => {
     it("should render the component", () => {
-        render(<CustomWidgetCountdown contentLanguage="en_US" message="World"/>);
+        render(<CustomWidgetCountdown contentLanguage="en_US" title={""} showtitle={false} titlecolor={""} countdowndate={""} expiredmessage={""} boxescolorbg={""} boxescolortext={""} boxescolorborder={""}/>);
 
-        expect(screen.getByText(/Hello World/)).toBeInTheDocument();
+        // expect(screen.getByText(/Hello World/)).toBeInTheDocument();
     })
 })
--- src/custom-widget-countdown.tsx
+++ src/custom-widget-countdown.tsx
@@ -18,7 +18,7 @@ export interface CustomWidgetCountdownProps extends BlockAttributes {
 
 export const CustomWidgetCountdown = ({ title, showtitle, titlecolor, boxescolorbg, boxescolorborder, boxescolortext, countdowndate, expiredmessage, contentLanguage }: CustomWidgetCountdownProps): ReactElement => {
 
-  return <div>
+  return <>
       <CountdownView
       {...{
         title,
@@ -32,15 +32,5 @@ export const CustomWidgetCountdown = ({ title, showtitle, titlecolor, boxescolor
         contentLanguage,
       }}
     />
-    <br /><br />
-    <div data-widget-conf-grid-type="100" data-widget-type="Section" data-widget-src="internal://staffbase.content.widgets.Section">
-      <div>
-        <div data-widget-conf-type="warning" data-widget-on-card="true" data-widget-type="Infobox" data-widget-src="internal://staffbase.content.widgets.Infobox">
-          <p>PROTOTYPES AND FOR DEMO PURPOSES ONLY!</p>
-          <p>NOT FOR PRODUCTION USE!</p>
-          <p>NOT AVAILABLE TO CUSTOMERS!</p>
-        </div>
-      </div>
-    </div>
-  </div>
+  </>
 };
